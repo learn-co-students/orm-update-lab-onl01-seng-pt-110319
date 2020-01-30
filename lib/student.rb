6189290f4f2834416943ff63c239077be477fc3e
github username <github email address>
2020-01-29 18:11:28 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,105 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  def initialize(id=nil, name:, grade:)
-    
+
+
+  def initialize
+
   end
 
+  def self.create_table
 
+  end
+
+  def self.drop_table
+   
+  end
+
+  def save
+ 
+  end
+
+  def update
+
+  end
+
+  def self.create
+
+  end
+
+  def self.new_from_db
+
+  end
+
+  def self.find_by_name
+
+  end
 end
+
+# require_relative "../config/environment.rb"
+
+# class Student
+
+#   attr_accessor :name, :grade
+#   attr_reader :id
+
+#   def initialize(id=nil, name, grade)
+#     @id = id
+#     @name = name
+#     @grade = grade
+#   end
+
+#   def self.create_table
+#     sql = <<-SQL
+#       CREATE TABLE IF NOT EXISTS students(
+#         id INTEGER PRIMARY KEY,
+#         name TEXT,
+#         grade TEXT)
+#         SQL
+#     DB[:conn].execute(sql)
+#   end
+
+#   def self.drop_table
+#     sql = <<-SQL
+#       DROP TABLE IF EXISTS students
+#     SQL
+#   DB[:conn].execute(sql)
+#   end
+
+#   def save
+#     if self.id
+#       self.update
+#     else
+#       sql = <<-SQL
+#         INSERT INTO students (name, grade)
+#         VALUES (?, ?)
+#       SQL
+#   DB[:conn].execute(sql, self.name, self.grade)
+#   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+#     end
+#   end
+
+#   def update
+#     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+#     DB[:conn].execute(sql, self.name, self.grade, self.id)
+#   end
+
+#   def self.create(name, grade)
+#     new_student = new(name, grade)
+#     new_student.save
+#     new_student
+#   end
+
+#   def self.new_from_db(row)
+#     id = row[0]
+#     name = row[1]
+#     grade = row[2]
+#     new(id, name, grade)
+#   end
+
+#   def self.find_by_name(name)
+#     sql = "SELECT * FROM students WHERE name = ?;"
+#     result = DB[:conn].execute(sql, name)[0]
+#     Student.new(result[0], result[1], result[2])
+#   end
+# end
